0aafd5876e80bd719c70941166f7e9559e9a5b74
lesley <lesleyiannarelli@gmail.com>
2019-11-01 17:11:39 -0400

    started on caregiver page

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,9 @@ import { Route, Switch, Redirect  } from 'react-router-dom';
 import Home from "./views/Home/Home"
 import NotFound from "./views/NotFound"
 import Header from "./components/Header/Header"
+import CaregiverTest from "./views/Caregiver/Caregiver"
+import "bootstrap/dist/css/bootstrap.min.css"
+
 
 
 const App = () => {
@@ -15,6 +18,7 @@ const App = () => {
           <Redirect to="/Home" />
         </Route>
         <Route component={NotFound}/>
+        <Route exact path="/CaregiverPage"> <CaregiverTest /></Route>
       </Switch>
     </div>
   );
