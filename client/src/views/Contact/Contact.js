afd8d558c7f07bb6bb63ae8f3a9da2bfd0d811c6
Kevin Kosta <kevinkosta@Kevins-MacBook-Pro-2.local>
2019-12-02 22:18:29 -0500

    updated contact

--- client/src/views/Contact/Contact.js
+++ client/src/views/Contact/Contact.js
@@ -12,33 +12,51 @@ class Contact extends React.Component {
         },
         zoom: 15
       };
+    constructor(props) {
+        super(props);
+        this.state = {
+          fname: '',
+          lname: '',
+          email: '',
+          message: '',
+        }
+      }
 
     render() {
         return (
+            
+
             <div className="contact">
             <p>Contact Me</p>
             
             <div>
+  <label>First Name</label>
+  <input type="text" id="fname" name="firstname" placeholder="Your name.."
+    value={this.state.fname}
+    onChange={e => this.setState({ fname: e.target.value })}
+  />
 
-            <form action="/action_page.php">
-            <label>First Name</label>
-            <input type="text" id="fname" name="firstname" placeholder="Your name.." />
+  <label>Last Name</label>
+  <input type="text" id="lname" name="lastname" placeholder="Your last name.."
+    value={this.state.lname}
+    onChange={e => this.setState({ lname: e.target.value })}
+  />
 
-            <label>Last Name</label>
-            <input type="text" id="lname" name="lastname" placeholder="Your last name.." />
-        
-        
-            <label>Email</label>
-            <input type="email" id="email" name="email" placeholder="Your email" />
+  <label>Email</label>
+  <input type="email" id="email" name="email" placeholder="Your email"
+    value={this.state.email}
+    onChange={e => this.setState({ email: e.target.value })}
+  />
 
 
-        
-            <label>Subject</label>
-            <textarea id="subject" name="subject" placeholder="Write something.."></textarea>
-            <input type="submit" value="Submit" />
-
+  <label>Message</label>
+  <textarea id="message" name="message" placeholder="Write something.."
+    onChange={e => this.setState({ message: e.target.value })}
+    value={this.state.message}
+  ></textarea>
+  <input type="submit" onClick={e => this.handleFormSubmit(e)} value="Submit" />
+           
             <div style={{ height: '100vh', width: '100%' }}>
-            
             <GoogleMapReact
               bootstrapURLKeys={{ key:"AIzaSyDnfmjZbEobwm3xYpaG-UIqqla-Dabc9gQ"}}
               defaultCenter={this.props.center}
@@ -46,16 +64,15 @@ class Contact extends React.Component {
             >
             </GoogleMapReact>
           </div>
-
-
-
-            </form>
             </div>
             </div>
-            
-        
           );
     }
+   
+    handleFormSubmit( event ) {
+        event.preventDefault();
+        console.log(this.state);
+      }
 
 }
 
