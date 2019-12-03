819e1f377b98d98217b43a853498094cc5a99ad8
Cooper Adams <coop.adams23@gmail.com>
2019-12-03 11:26:19 -0500

    tech page

--- client/src/views/Technology/Technology.js
+++ client/src/views/Technology/Technology.js
@@ -7,14 +7,27 @@ class Technology extends React.Component {
             <div className="grid-containerT">
             	<div className="bg1">
             		<div className="banner1">
-            			<h2>Evolving Outside the Box</h2>
+            			<h2>Technology</h2>
             		</div>
             	</div>
             	<div className="generalInfo">
             		<p>BioTork is the world leader in the use of evolutionary optimization to develop robust cellular factories capable of converting low value carbon sources such as biomass and agro-industrial by-products into high-value bio-based chemical commodities.</p>
-            		<p>Instead of adapting the production process to the metabolic limitations of the microorganisms, we adapt the microorganisms to perform optimally in the simplest and most economical production process.</p>
             	</div>
-            	<div className="techpic1"/>
+            	<div className="section2bg"/>
+            	<div className="section2">
+	            	<div className="techpic1">
+	            		<img src={require('../../assets/photos/microscope.jpg')} />
+	            	</div>
+	            	<div className="pictext1">
+	            		Instead of adapting the production process to the metabolic limitations of the microorganisms, we adapt the microorganisms to perform optimally in the simplest and most economical production process.
+	            	</div>
+            	</div>
+            	<div className="bg2">
+            		<div className="banner2">
+            			<h2>Evolving Outside the Box</h2>
+            		</div>
+            	</div>
+            	
             </div>
         );
     }
